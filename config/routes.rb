6b390a8c768211e34daa8324b04b2614ee5437e7
Marine Pages <marine.pages1@gmail.com>
2026-02-11 13:52:59 +0100

    CRUD for tasks sans css ok

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,20 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # 3) index - lister les tâches
+  get "tasks", to: "tasks#index", as: :tasks
+
+  # 5) new + create : je crée une nouvelle tâche
+  get  "tasks/new", to: "tasks#new", as: :new_task
+  post "tasks",     to: "tasks#create"
+
+  # 4) show (après /new) les détails d'une tâche
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  # 6) edit + update
+  get   "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id",      to: "tasks#update"
+
+  # 7) destroy
+  delete "tasks/:id", to: "tasks#destroy"
 end
